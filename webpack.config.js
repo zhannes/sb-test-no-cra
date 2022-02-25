9b054ed608499da2bf89712eb9caf43656057cbe
zhannes <zhannes@users.noreply.github.com>
2022-02-25 18:16:56 -0500

    remove webpack module federation related code

--- webpack.config.js
+++ webpack.config.js
@@ -1,55 +1,45 @@
 const HtmlWebpackPlugin = require("html-webpack-plugin");
-const ModuleFederationPlugin = require('webpack/lib/container/ModuleFederationPlugin');
 const path = require("path");
 const moduleFileExtensions = [
-  'web.mjs',
-  'mjs',
-  'web.js',
-  'js',
-  'web.ts',
-  'ts',
-  'web.tsx',
-  'tsx',
-  'json',
-  'web.jsx',
-  'jsx',
-  'scss'
+  "web.mjs",
+  "mjs",
+  "web.js",
+  "js",
+  "web.ts",
+  "ts",
+  "web.tsx",
+  "tsx",
+  "json",
+  "web.jsx",
+  "jsx",
+  "scss",
 ];
 
 module.exports = {
-  mode: 'development',
-  devServer: { port: '8083' },
+  mode: "development",
+  devServer: { port: "8083" },
   resolve: {
-    extensions: moduleFileExtensions.map(ext => `.${ext}`),
+    extensions: moduleFileExtensions.map((ext) => `.${ext}`),
   },
   module: {
     rules: [
       {
         test: /\.scss$/,
-        use: ["style-loader", "css-loader", "sass-loader"]
+        use: ["style-loader", "css-loader", "sass-loader"],
       },
       {
         test: /\.jsx?$/,
         exclude: /node_modules/,
-        use: ["babel-loader"]
-      }
-    ]
+        use: ["babel-loader"],
+      },
+    ],
   },
   optimization: {
-    splitChunks: { chunks: "all" }
+    splitChunks: { chunks: "all" },
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: path.resolve(__dirname, "src", "index.html")
+      template: path.resolve(__dirname, "src", "index.html"),
     }),
-    new ModuleFederationPlugin(
-      {
-        name: 'Team1',
-        filename: 'remoteEntry.js',
-  	    remotes: {
-          common_ui: 'common_ui@http://localhost:8081/remoteEntry.js',
-        },
-      }
-    ),
-  ]
+  ],
 };
